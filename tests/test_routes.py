16e3e797bd8f2abc8e068af37d4bab196885d31f
Jitesh Gosar <gosar95@gmail.com>
2023-01-10 03:32:14 -0500

    adds Delete service

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -179,3 +179,23 @@ class TestAccountService(TestCase):
         """It should not Update an Account that is not found"""
         resp = self.client.put(f"{BASE_URL}/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_delete_account(self):
+        """It should delete a single Account"""
+        account = self._create_accounts(1)[0]
+        response = self.client.get(
+            f"{BASE_URL}/{account.id}",
+            content_type="application/json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        response = self.client.delete(
+            f"{BASE_URL}/{account.id}",
+            content_type="application/json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    # def test_delete_account_not_found(self):
+    #     """It should not delete an Account that is not found"""
+    #     resp = self.client.delete(f"{BASE_URL}/0")
+    #     self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
